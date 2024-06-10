85b62a705c798483c245d5f53dd5623684bf4f5a
tirasjeffrey07 <tirasjeffrey2002@gmail.com>
2024-06-10 18:04:49 +0530

    All 3 Binary Tree traversals solved

---
+++ BinaryTreePostOrderTraversal.py
@@ -0,0 +1,41 @@
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+
+def postorderTraversal(root) -> list[int]:
+    trav = []
+    def postOrder(node):
+        if not node:
+            return
+
+        postOrder(node.left)
+        postOrder(node.right)
+        trav.append(node.val)
+
+    postOrder(root)
+    
+    return trav
+
+
+node1 = TreeNode(val = 12)
+node2 = TreeNode(val = 13)
+node3 = TreeNode(val = 14)
+node4 = TreeNode(val = 15)
+node5 = TreeNode(val = 16)
+node6 = TreeNode(val = 17)
+
+node1.left = node2
+node1.right = node3
+node3.left = node4
+node3.right = node5
+node2.left = node6
+node2.right = TreeNode(18)
+
+tree = node1
+
+print(postorderTraversal(tree))
